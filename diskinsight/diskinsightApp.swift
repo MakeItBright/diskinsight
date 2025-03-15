1984b78c43490715e371d6dbf2a78e3d19bc2744
j.breslauer <yb.apple.dev@gmail.com>
2025-03-15 20:44:14 +0100

    Merged local and remote changes

---
+++ diskinsight/diskinsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  diskinsightApp.swift
+//  diskinsight
+//
+//  Created by Yuri Breslavets on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct diskinsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ diskinsightTests/diskinsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  diskinsightTests.swift
+//  diskinsightTests
+//
+//  Created by Yuri Breslavets on 3/13/25.
+//
+
+import Testing
+@testable import diskinsight
+
+struct diskinsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

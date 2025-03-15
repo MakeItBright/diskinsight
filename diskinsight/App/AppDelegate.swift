166d56dbcddb8abec8f2f3c5e1d8bc2866af3acb
j.breslauer <yb.apple.dev@gmail.com>
2025-03-15 23:24:11 +0100

    upd: update structure, add fetcher

---
+++ diskinsight/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  diskinsight
+//
+//  Created by Yuri Breslavets on 3/15/25.
+//
+
+import Foundation
---
+++ diskinsight/ViewModels/DiskInfoFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  StorageFetcher.swift
+//  diskinsight
+//
+//  Created by Juri Breslauer on 3/15/25.
+//
+
+import Foundation
